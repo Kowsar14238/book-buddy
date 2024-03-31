daadd80eb04dc3a12e0e1c1009a861bdaa1a86a2
DESKTOP-DKVQBSG\User <kowsar15-14238@diu.edu.bd>
2024-03-31 06:17:51 +0600

    second commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,30 @@
-/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types"; // Import PropTypes
 
 const Card = ({ card }) => {
-  const { id, bookId, tags, bookName, author, category, rating } = card;
+  const { bookId, tags, bookName, author, category, rating, image } = card; // Destructure props
+
+  // Check if tags is an array before mapping
+  const renderTags = Array.isArray(tags) && tags.map((tag, index) => (
+    <button
+      key={index}
+      type="button"
+      className="px-5 py-2 font-semibold rounded-full text-[#23BE0A] bg-[#22be0a0a]"
+    >
+      {tag}
+    </button>
+  ));
 
   return (
     <div className="flex flex-col justify-between px-10 py-3 border-2 rounded-lg border-[#1313132d] transition hover:scale-105">
       <Link to={`/${bookId}`}>
         <div className="flex items-center justify-center">
-          <img className="h-40 w-30 rounded-lg" src={card.image} alt="" />
+          <img className="h-40 w-30 rounded-lg" src={image} alt={bookName} /> {/* Provide alt text */}
         </div>
 
-        <div className="flex flex-col  h-full ">
+        <div className="flex flex-col h-full">
           <div className="flex flex-wrap gap-3 py-2">
-            {tags.map((tag, index) => (
-              <button
-                key={index}
-                type="button"
-                className="px-5 py-2 font-semibold rounded-full text-[#23BE0A] bg-[#22be0a0a]"
-              >
-                {tag}
-              </button>
-            ))}
+            {renderTags} {/* Render tags conditionally */}
           </div>
 
           <div>
@@ -34,8 +37,8 @@ const Card = ({ card }) => {
                 <p>{rating}</p>
                 <img
                   className="h-3"
-                  src="../../public/star-outline-svgrepo-com.svg"
-                  alt=""
+                  src="../../public/star-outline-svgrepo-com.svg" // Verify image source
+                  alt="Star Icon"
                 />
               </div>
             </div>
@@ -44,6 +47,19 @@ const Card = ({ card }) => {
       </Link>
     </div>
   );
+};
+
+// Define propTypes to validate props
+Card.propTypes = {
+  card: PropTypes.shape({
+    bookId: PropTypes.number.isRequired,
+    tags: PropTypes.arrayOf(PropTypes.string), // Ensure tags is an array of strings
+    bookName: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Card;
